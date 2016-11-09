830e0f170e8e1e6b79c596c1dd5e5e6840fc1aef
heber mattos <feanorkun@hotmail.com>
2016-11-09 13:17:43 -0200

    Update Bill.bat

--- Bill.bat
+++ Bill.bat
@@ -1,11 +1,11 @@
-attrib -r "C:\Builds\7\Bill\CI - Bill\src\Bill\*.*" /s
+attrib -r "C:\Builds\7\Bill\CI_Bill\src\Bill\*.*" /s
 
-cd C:\Builds\7\Bill\CI - Bill\src\Bill
+cd C:\Builds\7\Bill\CI_Bill\src\Bill
 
-MSBuild.SonarQube.Runner.exe begin /version:1.0 /name:"Bill" /key:"Bill" /d:sonar.cs.opencover.reportsPaths="C:\Builds\7\Bill\CI - Bill\src\Bill\OpenCoverResults.xml";
+MSBuild.SonarQube.Runner.exe begin /version:1.0 /name:"Bill" /key:"Bill" /d:sonar.cs.opencover.reportsPaths="C:\BillTestResults\opencover.xml" /d:sonar.cs.nunit.reportsPaths="C:\BillTestResults\NUnitResults.xml" 
 
 "c:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild" bill.sln
 
-"C:\Builds\7\Bill\CI - Bill\src\Bill\packages\opencover.4.6.519\OpenCover.Console.exe" -register:user -target:"C:\Builds\7\Bill\CI - Bill\src\Bill\packages\NUnit.ConsoleRunner.3.5.0\tools\nunit3-console.exe" -targetargs:"\"C:\Builds\7\Bill\CI - Bill\src\Bill\test\RedeHost.Bill.Domain.Test\bin\Debug\RedeHost.Bill.Domain.Test.dll" -filter:"+[RedeHost.Bill.Domain]*" -output:"C:\Builds\7\Bill\CI - Bill\src\Bill\OpenCoverResults.xml" 
+"C:\Builds\7\Bill\CI_Bill\src\Bill\packages\opencover.4.6.519\OpenCover.Console.exe" -register:user -target:"C:\Builds\7\Bill\CI_Bill\src\Bill\packages\NUnit.ConsoleRunner.3.5.0\tools\nunit3-console.exe" -targetargs:"-result:\BillTestResults\NUnitResults.xml;format=nunit2 C:\Builds\7\Bill\CI_Bill\src\Bill\test\RedeHost.Bill.Domain.Test\bin\Debug\RedeHost.Bill.Domain.Test.dll" -filter:"+[RedeHost.Bill.Domain]*" -output:"C:\BillTestResults\opencover.xml" 
 
 MSBuild.SonarQube.Runner.exe end
